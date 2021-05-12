59de03357f5d2991a8b413c2148a61469b682d6e
Luiz Fernando <lfaires@gmail.com>
2021-05-11 21:40:23 -0300

    componentização App

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./Header"
 
 export default function App(){
     return (
+        <>
         <Header />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Movies />
+                </Route>
+                <Route path="/movie/:idMovie" exact>
+                    <SelectTime />
+                </Route>
+                <Route path="/movietime/:idTime" exact>
+                    <SelectSeat />
+                </Route>
+                <Route path="/congrats" exact>
+                    <Tickets />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+        </>
     )
 }
