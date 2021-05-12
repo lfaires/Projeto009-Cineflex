df3522f117eebd4aa380a40692d0e578b4038711
Luiz Fernando <lfaires@gmail.com>
2021-05-11 23:06:07 -0300

    componente Movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Movies(){
+    const [movies, setMovies] = useState([]);    
+    
+    useEffect( () => {
+        const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/movies");
+
+        request.then( response => {setMovies(response.data)});
+
+    },[])
+       console.log("lista de filmes:",movies)
     return (
         <div className="container">
             <h2>Selecione o filme</h2>
             <ul className="listMovies">
-   
+               { movies.map( movie => 
+               <li key={movie.id}> <img src={movie.posterURL} alt={movie.title + " poster"} /> </li>)} 
             </ul>
         </div>
     )
