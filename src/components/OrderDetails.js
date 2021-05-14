b92dc4d18c7dbd6d5cc150f1bddc27035fc978c7
Luiz Fernando <lfaires@gmail.com>
2021-05-14 17:46:21 -0300

    formatação cpf na última tela

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function OrderDetails(props) {
     const {seats, movie, name, cpf, setHome} = props
+    const newCpf = cpf.substring(0,3) + "." + cpf.substring(3,6) + "." + cpf.substring(6,9) + "-" + cpf.substring(9,11)
     setHome(true)
 
     return (
@@ -20,7 +21,7 @@ export default function OrderDetails(props) {
                 <Subheading>Comprador</Subheading>
                 <Details>
                     <Detail>{"Nome: " + name}</Detail>
-                    <Detail>{"CPF: " + cpf}</Detail>
+                    <Detail>{"CPF: " + newCpf}</Detail>
                 </Details>
                 <Link to="/">
                     <Button>Voltar para Home</Button>
